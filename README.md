469ed8cd4bdf0b471d905436dc4aba57b0845977
why <wuhuiyun@hoodinn.com>
2020-04-13 00:27:06 +0800

    commit build script

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ffmpeg-android
-ffmpeg build script
+>+ 支持最新版本的ndk环境
+>+ 可以将ffmpeg生成为一个so文件
---
+++ build_x86_64.sh
@@ -0,0 +1,126 @@
+# MIT License
+
+# Copyright (c) 2020 Huiyun Wu
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+#!/bin/bash
+echo "进入编译ffmpeg脚本"
+
+export NDK=/mnt/c/developer/Android/android-ndk-r20b
+#5.0
+export API_LEVEL=21
+export PLATFORM=$NDK/platforms/android-$API_LEVEL/arch-x86_64
+export TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/linux-x86_64
+export SYSROOT=$NDK/sysroot
+export ASM_INCLUDE=$SYSROOT/usr/include/x86_64-linux-android
+export CPU=x86_64
+export PREFIX=android/$CPU
+# 为了启用yasm
+export PATH=$PATH:$TOOLCHAIN/bin
+
+echo "开始编译ffmpeg"
+CFLAG="-I$ASM_INCLUDE -I$SYSROOT/usr/include -isysroot $SYSROOT -fPIC -DANDROID -D__ANDROID_API__=$API_LEVEL -Dipv6mr_interface=ipv6mr_ifindex -fasm -fomit-frame-pointer -march=k8 "
+EXTRA_PARMAS="--enable-yasm \
+    --enable-jni \
+    --enable-mediacodec"
+
+./configure \
+--prefix=$PREFIX \
+--target-os=android \
+--cross-prefix=$TOOLCHAIN/bin/x86_64-linux-android- \
+--arch=$CPU \
+--sysroot=$PLATFORM \
+--extra-cflags="$CFLAG" \
+--cc=$TOOLCHAIN/bin/x86_64-linux-android21-clang \
+--nm=$TOOLCHAIN/bin/x86_64-linux-android-nm \
+--ld=$TOOLCHAIN/bin/x86_64-linux-android-ld \
+--as=$TOOLCHAIN/bin/x86_64-linux-android-as \
+--disable-shared \
+--enable-runtime-cpudetect \
+--enable-gpl \
+--enable-small \
+--enable-cross-compile \
+--disable-debug \
+--enable-static \
+--disable-doc \
+--disable-ffmpeg \
+--disable-ffplay \
+--disable-ffprobe \
+--disable-postproc \
+--disable-avdevice \
+--disable-symver \
+--disable-stripping \
+$EXTRA_PARMAS
+
+# 由于mac上的sed命令和linux下的sed的参数有差异，所以mac下需要使用下面的参数
+# file_path="''"
+# linux上使用
+file_path=""
+
+fix_definitions=(
+    "HAVE_CBRT"
+    "HAVE_CBRTF"
+    "HAVE_COPYSIGN"
+    "HAVE_ERF"
+    "HAVE_ISFINITE"
+    "HAVE_HYPOT"
+    "HAVE_RINT"
+    "HAVE_LRINT"
+    "HAVE_LRINTF"
+    "HAVE_ROUND"
+    "HAVE_ROUNDF"
+    "HAVE_TRUNC"
+    "HAVE_TRUNCF"
+    "HAVE_ISNAN"
+    "HAVE_LOCALTIME_R"
+    "HAVE_GMTIME_R"
+    "HAVE_INET_ATON"
+)
+
+sed -i $file_path "/#define getenv(x) NULL/d" config.h
+
+for str in ${fix_definitions[@]}; do
+    eval "sed -i $file_path 's/$str 0/$str 1/g' config.h"
+done
+
+# 清除compat下的.o主要针对stdatomic.o
+find compat -name "*.o" | xargs rm -f
+make clean
+make -j8
+make install
+
+# 将几个静态库链接为一个动态库
+$TOOLCHAIN/bin/x86_64-linux-android-ld \
+-rpath-link=$PLATFORM/usr/lib \
+-L$PLATFORM/usr/lib64 \
+-L$PREFIX/lib \
+-soname libffmpeg.so -shared -nostdlib -Bsymbolic --whole-archive --no-undefined -o \
+$PREFIX/libffmpeg.so \
+    libavutil/libavutil.a \
+    libavcodec/libavcodec.a \
+    libavfilter/libavfilter.a \
+    libswresample/libswresample.a \
+    libavformat/libavformat.a \
+    libswscale/libswscale.a \
+    -lc -lm -lz -ldl -llog --dynamic-linker=/system/bin/linker \
+    $TOOLCHAIN/lib/gcc/x86_64-linux-android/4.9.x/libgcc_real.a \
+
+# strip
+$TOOLCHAIN/bin/x86_64-linux-android-strip $PREFIX/libffmpeg.so
